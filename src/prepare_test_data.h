d03c830d4a7146b63f5b3b76db59d30b9d2d9117
Karol Kontny <barolek@gmail.com>
2020-12-05 20:55:49 +0100

    Starting mat4 tests

--- src/prepare_test_data.h
+++ src/prepare_test_data.h
@@ -31,3 +31,18 @@ std::vector<T> prepare_test_data_init_list(std::size_t size) {
 
     return testData;
 }
+
+template<typename T>
+std::vector<T> prepare_mat4_test_data(std::size_t size) {
+    std::vector<T> testData(size);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> distribution(0.0f, 10.0f);
+
+    for (auto& vec : testData) {
+        vec = T(distribution(gen), distribution(gen), distribution(gen), distribution(gen), distribution(gen), distribution(gen), distribution(gen), distribution(gen), distribution(gen),distribution(gen),distribution(gen),distribution(gen),distribution(gen),distribution(gen),distribution(gen),distribution(gen));
+    }
+
+    return testData;
+}
